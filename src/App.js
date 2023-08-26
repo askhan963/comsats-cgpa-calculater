7c2f9417602029a472545430174a4e2c89eee67b
askhan963 <awaisknas963@gmail.com>
2023-08-26 11:45:16 +0500

    React Router Dom Installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import GpaCalculator from "./components/GpaCalculator";
 function App() {
   return (
-    <div className="text-5xl bg-black text-white p-5 text-center">
+    <>
+     <div className="text-5xl bg-black text-white p-5 text-center">
      Hello React.js
     </div>
+    <GpaCalculator/>
+    </>
+   
   );
 } 
 
---
+++ src/components/GpaCalculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GpaCalculator = () => {
+  return (
+    <div className='text-8xl bg-black text-white '>
+        GpaCalculator
+        </div>
+  )
+}
+
+export default GpaCalculator
